81344024d06a70519564d54ce455d3b04c04ba49
Jonny Weese <jonny@cs.jhu.edu>
2018-06-13 13:53:27 -0400

    alignment: throw when no strtol conversion

--- src/alignment.cpp
+++ src/alignment.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <stdexcept>
+#include <string>
 
 namespace jhu::thrax {
 
@@ -11,8 +13,14 @@ Alignment readAlignment(std::string_view sv) {
   char* e;
   for (auto& p : result) {
     p.src = std::strtol(s, &e, 10);
+    if (e == s) {
+      throw std::invalid_argument(std::string(sv));
+    }
     s = e;
     p.tgt = -std::strtol(s, &e, 10);
+    if (e == s) {
+      throw std::invalid_argument(std::string(sv));
+    }
     s = e;
   }
   return result;
