92526261bfc561ae22257d9f9bb371f205431c0d
Jonny Weese <jonny@cs.jhu.edu>
2018-06-14 10:53:10 -0400

    Hiero labeler

---
+++ src/label.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "phrase.h"
+#include "sentence.h"
+
+namespace jhu::thrax {
+
+class Labeler {
+ public:
+  virtual std::string operator()(
+      const AlignedSentencePair& sentence, SpanPair nt) const = 0;
+  virtual ~Labeler() = default;
+};
+
+class HieroLabeler : Labeler {
+ public:
+  std::string operator()(const AlignedSentencePair&, SpanPair) const override {
+    return "X";
+  }
+};
+
+}
