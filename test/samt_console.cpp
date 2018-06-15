491c95ec1877685f79508d5156c528d2741ce94a
Jonny Weese <jonny@cs.jhu.edu>
2018-06-15 16:08:28 -0400

    catch exceptions in SAMT console
    Since empty parses are common in my example data.

--- test/samt_console.cpp
+++ test/samt_console.cpp
@@ -8,11 +8,15 @@
 int main() {
   std::string line;
   while (std::getline(std::cin, line)) {
-    auto asp = jhu::thrax::readAlignedSentencePair<false, true>(line);
-    auto tree = jhu::thrax::readTree(jhu::thrax::fields(line)[1]);
-    jhu::thrax::SAMTLabeler samt{std::move(tree)};
-    for (const auto& rule : jhu::thrax::extract(asp, 10)) {
-      std::cout << jhu::thrax::LabeledRuleView{ rule, samt } << '\n';
+    try {
+      auto asp = jhu::thrax::readAlignedSentencePair<false, true>(line);
+      auto tree = jhu::thrax::readTree(jhu::thrax::fields(line)[1]);
+      jhu::thrax::SAMTLabeler samt{std::move(tree)};
+      for (const auto& rule : jhu::thrax::extract(asp, 10)) {
+        std::cout << jhu::thrax::LabeledRuleView{ rule, samt } << '\n';
+      }
+    } catch (std::exception& e) {
+      std::cerr << e.what() << ' ' << line << '\n';
     }
   }
 }
