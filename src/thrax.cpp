fd60701d644dbfbc642972a729dfbadf93c71ef8
Jonny Weese <jonny@cs.jhu.edu>
2018-06-15 17:06:08 -0400

    add ineffective multithreading
    Need to debug for sure.

--- src/thrax.cpp
+++ src/thrax.cpp
@@ -1,5 +1,7 @@
+#include <future>
 #include <iostream>
 #include <mutex>
+#include <vector>
 
 #include "label.h"
 #include "phrasalrule.h"
@@ -22,18 +24,16 @@ bool process() {
       return false;
     }
   }
-  auto asp = jhu::thrax::readAlignedSentencePair<false, true>(line);
-  auto tree = jhu::thrax::readTree(jhu::thrax::fields(line)[1]);
-  jhu::thrax::SAMTLabeler samt{std::move(tree)};
   try {
-    {
-      std::unique_lock<std::mutex> g(outputLock, std::defer_lock_t{});
-      if constexpr (Lock) {
-        g.lock();
-      }
-      for (const auto& rule : jhu::thrax::extract(asp, 10)) {
-        std::cout << jhu::thrax::LabeledRuleView{ rule, samt } << '\n';
-      }
+    auto asp = jhu::thrax::readAlignedSentencePair<false, true>(line);
+    auto tree = jhu::thrax::readTree(jhu::thrax::fields(line)[1]);
+    jhu::thrax::SAMTLabeler samt{std::move(tree)};
+    std::unique_lock<std::mutex> g(outputLock, std::defer_lock_t{});
+    if constexpr (Lock) {
+      g.lock();
+    }
+    for (const auto& rule : jhu::thrax::extract(asp, 10)) {
+      std::cout << jhu::thrax::LabeledRuleView{ rule, samt } << '\n';
     }
   } catch (std::exception& e) {
     std::cerr << e.what() << ' ' << line << '\n';
@@ -43,7 +43,22 @@ bool process() {
 
 }  // namespace
 
-int main() {
+int main(int argc, char** argv) {
+  int threads = 1;
+  if (argc > 1) {
+    threads = std::atoi(argv[1]);
+  }
   std::ios::sync_with_stdio(false);
-  while (process<false>()) {}
+  if (threads < 2) {
+    while (process<false>()) {}
+    return 0;
+  }
+  std::vector<std::future<void>> workers;
+  workers.reserve(threads);
+  for (int i = 0; i < threads; i++) {
+    workers.push_back(std::async([]() { while (process<true>()) {} }));
+  }
+  for (auto& f : workers) {
+    f.get();
+  }
 }
