81e8715f93f11b71016e05dd03b8c385140ac580
Jonny Weese <jonny@cs.jhu.edu>
2018-06-15 13:55:40 -0400

    small tree test

--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -3,6 +3,18 @@
 
 namespace jhu::thrax {
 
+namespace {
+
+void expectNode(
+    IndexType s, IndexType e, const char*l, const Node* n, Node actual) {
+  Span sp{ s, e };
+  EXPECT_EQ(sp, actual.span);
+  EXPECT_EQ(l, actual.label);
+  EXPECT_EQ(n, actual.parent);
+}
+
+}
+
 TEST(TreeTests, EmptyString) {
   auto t = readTree("");
   EXPECT_EQ(0, t.size());
@@ -13,9 +25,15 @@ TEST(TreeTests, OneNode) {
   ASSERT_EQ(1, t.size());
   auto root = t.front();
   auto s = Span{0, 1};
-  EXPECT_EQ(s, root.span);
-  EXPECT_EQ("ROOT", root.label);
-  EXPECT_EQ(nullptr, root.parent);
+  expectNode(0, 1, "ROOT", nullptr, root);
+}
+
+TEST(TreeTests, Small) {
+  auto t = readTree("(ROOT (A foo) (B bar))");
+  ASSERT_EQ(3, t.size());
+  expectNode(0, 2, "ROOT", nullptr, t[0]);
+  expectNode(0, 1, "A", &t[0], t[1]);
+  expectNode(1, 2, "B", &t[0], t[2]);
 }
 
 }
