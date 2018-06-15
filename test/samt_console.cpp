07a0241c72eccacf7285700c438b42af061889f5
Jonny Weese <jonny@cs.jhu.edu>
2018-06-15 15:30:27 -0400

    SAMT console for testing

---
+++ test/samt_console.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "label.h"
+#include "phrasalrule.h"
+#include "sentence.h"
+#include "tree.h"
+
+int main() {
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    auto asp = jhu::thrax::readAlignedSentencePair<false, true>(line);
+    auto tree = jhu::thrax::readTree(jhu::thrax::fields(line)[1]);
+    jhu::thrax::SAMTLabeler samt{std::move(tree)};
+    for (const auto& rule : jhu::thrax::extract(asp, 10)) {
+      std::cout << jhu::thrax::LabeledRuleView{ rule, samt } << '\n';
+    }
+  }
+}
