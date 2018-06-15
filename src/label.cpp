2083ecac4975ff0e1c6578fbd4fd8e47f70b2405
Jonny Weese <jonny@cs.jhu.edu>
2018-06-15 14:15:00 -0400

    SAMT label type

--- src/label.cpp
+++ src/label.cpp
@@ -1,6 +1,42 @@
 #include "label.h"
 
+#include <variant>
+
 namespace jhu::thrax {
+
+namespace {
+
+struct Constituent {
+  std::string_view label;
+};
+
+struct ForwardApply {
+  // "result/arg"
+  std::string_view result, arg;
+};
+
+struct BackwardApply {
+  // "result\arg"
+  std::string_view result, arg;
+};
+
+struct Concat {
+  std::string_view a, b;
+};
+
+struct DoubleConcat {
+  std::string_view a, b, c;
+};
+
+using SAMTLabel = std::variant<
+  Constituent,
+  ForwardApply,
+  BackwardApply,
+  Concat,
+  DoubleConcat
+>;
+
+}  // namespace
 
 std::string SAMTLabeler::operator()(SpanPair) const {
   return "";
