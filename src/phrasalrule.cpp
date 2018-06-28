5cd24a7db72924523025f861f9df78a3b49cffeb
Jonny Weese <jonny@cs.jhu.edu>
2018-06-28 10:25:55 -0400

    rip out ContainedNT stuff

--- src/phrasalrule.cpp
+++ src/phrasalrule.cpp
@@ -7,7 +7,7 @@ namespace jhu::thrax {
 namespace {
 
 std::optional<PhrasalRule> addNonterminal(const PhrasalRule& r, const NT& nt) {
-  if (r.lhs.span == nt.span) {
+  if (r.lhs.span == nt.span || !r.lhs.span.contains(nt.span)) {
     return {};
   }
   auto begin = r.nts.begin();
@@ -30,18 +30,18 @@ std::optional<PhrasalRule> addNonterminal(const PhrasalRule& r, const NT& nt) {
 
 using Rules = std::vector<PhrasalRule>;
 
-Rules addAllNonterminals(
-    const Rules& rules, const std::vector<ContainedNTs>& phrases) {
+Rules addAllNonterminals(const Rules& rules, const std::vector<NT>& phrases) {
   Rules next;
   next.reserve(rules.size());
-  ContainedNTs key;
   for (const auto& rule : rules) {
-    key.lhs = rule.lhs;
+    auto rest = SpanPair{rule.remainingSource(), {}};
     auto it = std::lower_bound(
-        phrases.begin(), phrases.end(), key, byLhsSpan);
-    for (auto p : it->contained) {
-      if (auto r = addNonterminal(rule, *p); r.has_value()) {
+        phrases.begin(), phrases.end(), rest, bySourceStart);
+    for (; it < phrases.end(); it++) {
+      if (auto r = addNonterminal(rule, *it); r.has_value()) {
         next.push_back(*std::move(r));
+      } else if (it->span.src.start >= rest.src.end) {
+        break;
       }
     }
   }
@@ -80,18 +80,13 @@ std::vector<PhrasalRule> extract(
       initial.end(),
       nts.begin(),
       [&labeler](auto p) { return NT(p, labeler(p)); });
-  std::vector<ContainedNTs> contained;
-  contained.reserve(nts.size());
-  for (auto nt : nts) {
-    contained.emplace_back(nts, std::move(nt));
-  }
-  std::sort(contained.begin(), contained.end(), byLhsSpan);
-  rules.front().reserve(contained.size());
-  for (const auto& c : contained) {
-    rules.front().emplace_back(sentence, c.lhs);
+  std::sort(nts.begin(), nts.end(), bySourceSpan);
+  rules.front().reserve(nts.size());
+  for (const auto& nt : nts) {
+    rules.front().emplace_back(sentence, nt);
   }
   for (size_t i = 0; i < kMaxNonterminals; i++) {
-    rules[i + 1] = addAllNonterminals(rules[i], contained);
+    rules[i + 1] = addAllNonterminals(rules[i], nts);
   }
   return cat(std::move(rules));
 }
